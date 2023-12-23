dbc155e1ec01484d82ddacfb4db918a5522da83d
Mlowegene <genemlowe@gmail.com>
2023-12-23 13:02:39 +0300

    feat:  add feature that change font family

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,35 @@
 import logo from '../assets/logo.svg';
 import moon from '../assets/icon-moon.svg';
 import { FaToggleOn, FaToggleOff } from 'react-icons/fa';
+import { ThemeContext } from "../App";
+import { useContext } from "react";
 
-function NavBar() {
+function NavBar({selectedFont, setSelectedFont}) {
+
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  const handleFontChange = (e) => {
+    setSelectedFont(e.target.value);
+  };
+
+  
+
   return (
     <div className="nav">
         <img src={logo} alt="logo" className='logo' />
-        <select className='font-type'>
-            <option>Sans Serif</option>
-            <option>Serif</option>
-            <option>Mono</option>
+        <select className='font-type' onChange={handleFontChange} value={selectedFont}>
+            <option value="Inter">Sans Serif</option>
+            <option value="Lora">Serif</option>
+            <option value="Inconsolata">Mono</option>
         </select>
         <div className='toggle'>
-            <FaToggleOff className='toggle-icon' />
-            <img src={moon} alt='moon-icon' />
+            <span onClick={toggleTheme} className='toggle-icon'>
+              {theme === "dark" ? <FaToggleOn /> : <FaToggleOff />}
+            </span>
+            <img src={moon} alt='moon-icon' className='moon-icon' />
         </div>
     </div>
   )
 }
 
-export default NavBar
+export default NavBar;
